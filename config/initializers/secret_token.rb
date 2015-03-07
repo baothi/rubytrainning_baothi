ef3c1b70a573baf4c7eacf79dd1ec7e82fd8b0b2
baothi246@gmail.com <baothi246@gmail.com>
2015-03-07 23:25:45 +0700

    ruby trainning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubytrainningBaothi::Application.config.secret_key_base = '8f683753c1ebdf98792d01fc24edb645ff101d4bf55dcb8950f29599364ab010b026af4acbb6195ecd166c661e486c53828f612a2b8e3ef3b53e8662b30d7cc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubytrainningBaothi::Application.config.session_store :cookie_store, key: '_rubytrainning_baothi_session'
